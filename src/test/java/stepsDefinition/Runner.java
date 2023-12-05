12051f934f2c8cfbbcf64d5287d1bd79c62ba28b
Odorsaf <dothmen@dom.tti>
2023-12-05 11:28:35 +0100

    mess

--- src/test/java/stepsDefinition/Runner.java
+++ src/test/java/stepsDefinition/Runner.java
@@ -5,10 +5,13 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
  
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", glue = { "stepsDefinition" }, monochrome = true, plugin = {
-		"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json","json:target/rapport.json",
-		"junit:target/cucumber-results.xml" }, tags = "")
+@RunWith(Cucumber.class) 
+@CucumberOptions (
+		features="src/test/resources/features",
+		glue= {"stepsDefinition"},
+		monochrome=true,
+		plugin = {
+				"pretty", "html:target/rapport.html", "json:target/rapport.json"
+		})
 public class Runner {
- 
 }
